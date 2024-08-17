9437f3cbb36ba7c26578fa1104ff43f2229ec367
far1h <farihmuhammad45@gmail.com>
2024-08-17 14:02:51 +0700

    Initial Commit

---
+++ RetirementCalculator/RetirementCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetirementCalculatorApp.swift
+//  RetirementCalculator
+//
+//  Created by Farih Muhammad on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct RetirementCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

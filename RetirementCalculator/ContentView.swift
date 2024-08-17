cf8ab62221e5c49a0940f4a175c4493fa106d3a4
far1h <farihmuhammad45@gmail.com>
2024-08-17 15:28:33 +0700

    Create UI

--- RetirementCalculator/ContentView.swift
+++ RetirementCalculator/ContentView.swift
@@ -6,14 +6,38 @@
 //
 
 import SwiftUI
+import AppCenterCrashes
 
 struct ContentView: View {
+    @State private var monthlyInvestments = ""
+    @State private var currentAge = ""
+    @State private var retireAge = ""
+    @State private var interestRate = ""
+    @State private var currentSavings = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack (alignment: .leading) {
+            TextField("Monthly Investments", text: $monthlyInvestments)                .textFieldStyle(.roundedBorder)
+
+            TextField("Your current age", text: $currentAge)                .textFieldStyle(.roundedBorder)
+
+            TextField("Your planned retirement age", text: $retireAge)                .textFieldStyle(.roundedBorder)
+
+            TextField("Average interest rate of investments", text: $interestRate)
+                .textFieldStyle(.roundedBorder)
+
+            TextField("Current savings", text: $currentSavings)
+                .textFieldStyle(.roundedBorder)
+            Button {
+
+            } label: {
+                Spacer()
+                Text(
+                    "Calculate retirement amount"
+                )
+                Spacer()
+            }.buttonStyle(.borderedProminent)
+            Text("Enter some values to calculate your retirement amount")
+            Spacer()
         }
         .padding()
     }

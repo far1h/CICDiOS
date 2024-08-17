62f02a21afb1f79f14513ad5f88770ecb12fb544
far1h <farihmuhammad45@gmail.com>
2024-08-17 18:22:15 +0700

    Calculate retirement amount

--- RetirementCalculator/ContentView.swift
+++ RetirementCalculator/ContentView.swift
@@ -10,6 +10,8 @@ import AppCenterCrashes
 import AppCenterAnalytics
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    
     @State private var monthlyInvestments = ""
     @State private var currentAge = ""
     @State private var retireAge = ""
@@ -18,7 +20,20 @@ struct ContentView: View {
     @State private var result = "Enter some values to calculate your retirement amount"
     @State private var showAlert = false // Added state to manage alert presentation
     
-    
+    // MARK: - FUNCTIONS
+    func calculateRetirementAmount(current_age: Int, retirement_age : Int, monthly_investment: Float, current_savings: Float, interest_rate: Float) -> Double {
+             let months_until_retirement = (retirement_age - current_age) * 12
+
+             var retirement_amount = Double(current_savings) * pow(Double(1+interest_rate/100), Double(months_until_retirement))
+
+             for i in 1...months_until_retirement {
+                 let monthly_rate = interest_rate / 100 / 12
+                 retirement_amount += Double(monthly_investment) * pow(Double(1+monthly_rate), Double(i))
+             }
+
+             return retirement_amount
+         }
+    // MARK: - BODY
     var body: some View {
         VStack (alignment: .leading) {
             TextField("Monthly Investments", text: $monthlyInvestments)                .textFieldStyle(.roundedBorder)
@@ -34,7 +49,9 @@ struct ContentView: View {
                 .textFieldStyle(.roundedBorder)
             Button {
                 //                Crashes.generateTestCrash()
-                result = "If you save $\(Double(monthlyInvestments)!) every month for \(Int(retireAge)! - Int(currentAge)!) years, and invest that money plus your current investment of $\(Double(currentSavings)!) at a \(Double(interestRate)!)% anual interest rate, you will have $X by the time you are \(Int(retireAge)!)"
+                
+                let retirement_amount = calculateRetirementAmount(current_age: Int(currentAge) ?? 0, retirement_age: Int(retireAge) ?? 0, monthly_investment: Float(monthlyInvestments) ?? 0, current_savings: Float(currentSavings) ?? 0, interest_rate: Float(interestRate) ?? 0)
+                result = "If you save $\(Double(monthlyInvestments)!) every month for \(Int(retireAge)! - Int(currentAge)!) years, and invest that money plus your current investment of $\(Double(currentSavings)!) at a \(Double(interestRate)!)% anual interest rate, you will have $\(retirement_amount.rounded()) by the time you are \(Int(retireAge)!)"
                 
                 let properties = ["current_age": currentAge,
                                   "planned_retirement_age": retireAge]
@@ -64,6 +81,8 @@ struct ContentView: View {
         }
     }
 }
+
+// MARK: - PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

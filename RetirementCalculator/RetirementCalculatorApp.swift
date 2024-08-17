a0a7361a8f6f823dd8ac61b49242b3c372f8fd4b
far1h <farihmuhammad45@gmail.com>
2024-08-17 14:27:33 +0700

    Add AppCenter frameworks

--- RetirementCalculator/RetirementCalculatorApp.swift
+++ RetirementCalculator/RetirementCalculatorApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @main
 struct RetirementCalculatorApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+          AppCenter.start(withAppSecret: "d89a8595-7280-4169-ab93-ffbd634344ed", services:[
+            Crashes.self, Analytics.self])
+        return true
     }
 }
